4cdd5fea33186028b8944d1a135b64b7d06a709e
Vladimir Tolkachev <VladimirTolkachev@users.noreply.github.com>
2020-03-23 12:50:24 +0300

    two variants

---
+++ threadTest/Data.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace threadTest
+{
+    static class Data
+    {
+        public static EventHandler ValueChanged = delegate { };
+        private static bool _A;
+        public static bool A
+        {
+            get { return _A; }
+            set
+            {
+                _A = value;
+                ValueChanged(null, EventArgs.Empty);
+            }
+        }
+
+    }
+}
---
+++ threadTest/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace threadTest
+{
+    #region подписка на событие
+    //public partial class Form1 : Form
+    //{
+    //    static bool flag = true;
+    //    delegate void DelUpdate();
+
+    //    public Form1()
+    //    {
+    //        InitializeComponent();
+    //    }
+
+    //    private void Form1_Load(object sender, EventArgs e)
+    //    {
+    //        Data.ValueChanged += upd;
+
+    //        System.Timers.Timer test_timer = new System.Timers.Timer();
+    //        test_timer.Interval = 100;
+    //        test_timer.Elapsed += timer1_Tick;
+    //        test_timer.Start();
+
+    //        Data.A = true;
+    //    }
+
+    //    private void timer1_Tick(object sender, EventArgs e)
+    //    {
+    //        Data.A = !Data.A;
+    //    }
+
+    //    public void upd()
+    //    {
+    //        radioButton1.Checked = Data.A;
+    //    }
+    //    public void upd(object sender, EventArgs e)
+    //    {
+    //        DelUpdate _delUpdate = upd;
+    //        radioButton1.Invoke(_delUpdate);
+    //    }
+
+    //    private void button1_Click(object sender, EventArgs e)
+    //    {
+    //        if (flag)
+    //        {
+    //            flag = false;
+    //        }
+    //        else
+    //        {
+    //            flag = true;
+    //        }
+    //    }
+    //}
+    #endregion
+
+    #region только через делегат
+    public partial class Form1 : Form
+    {
+        static bool flag = true;
+        delegate void DelUpdate();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Data.ValueChanged += upd;
+
+
+            System.Timers.Timer test_timer = new System.Timers.Timer();
+            test_timer.Interval = 2000;
+            test_timer.Elapsed += timer1_Tick;
+            test_timer.Start();
+
+            Data.A = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //DelUpdate _delUpdate = upd;
+
+            Data.A = !Data.A;
+            //radioButton1.Invoke(_delUpdate);
+        }
+
+        public void upd()
+        {
+            radioButton1.Checked = Data.A;
+        }
+        public void upd(object sender, EventArgs e)
+        {
+            //radioButton1.Checked = Data.A;
+            DelUpdate _delUpdate = upd;
+            radioButton1.Invoke(_delUpdate);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (flag)
+            {
+                flag = false;
+            }
+            else
+            {
+                flag = true;
+            }
+        }
+    }
+    #endregion
+}
